85d5f07d59fd372831451d207747098cb8bfe4c8
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-01-20 14:33:21 +0000

    bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,11 @@ class BooksController < ApplicationController
   
   def create #保存機能（投稿が成功したら投稿したidの詳細ページへ、失敗したら投稿フォームにエラーを出す）
    @book = Book.new(book_params)
-   @book.save
-   redirect_to book_path(@book.id)
+   if @book.save
+    redirect_to book_path(@book.id)
+   else
+    render :index
+   end
   end
 
   def show #詳細ページ
@@ -16,6 +19,23 @@ class BooksController < ApplicationController
   end
 
   def edit #編集ページ
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+   @book = Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(book.id)
+   else
+    render :index
+   end
+  end
+
+  
+  def destroy
+   @book = Book.find(params[:id])
+   @book.destroy
+   redirect_to'/books'
   end
 
 private
